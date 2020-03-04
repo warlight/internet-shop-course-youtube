bc8c0486b36ab498a3b95dd8c58b3abf551512a8
roman <romansdavydov@gmail.com>
2020-03-04 20:25:31 +0200

    fix seeder for Currencies

--- database/seeds/CurrencySeeder.php
+++ database/seeds/CurrencySeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class CurrencySeeder extends Seeder
@@ -19,18 +20,24 @@ class CurrencySeeder extends Seeder
                 'symbol' => '₽',
                 'is_main' => 1,
                 'rate' => 1,
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
             ],
             [
                 'code' => 'USD',
                 'symbol' => '$',
                 'is_main' => 0,
                 'rate' => 0,
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
             ],
             [
                 'code' => 'EUR',
                 'symbol' => '€',
                 'is_main' => 0,
                 'rate' => 0,
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
             ],
         ]);
     }
